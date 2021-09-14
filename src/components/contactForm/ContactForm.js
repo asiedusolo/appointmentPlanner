d10815bc529a66258b4d3ec23ba054744b33a64a
asiedusolo <solomon.asiedu@amalitech.org>
2021-09-14 15:57:24 -0400

    adding regex pattern to phone input

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -12,7 +12,7 @@ export const ContactForm = ({
   return (
     <form onSubmit={handleSubmit}>
         <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Full name" />
-        <input type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} placeholder="Phone number" />
+        <input type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} placeholder="Phone number" pattern="(0|\+233)([25])([0456])[ -]?(\d{3})[ -]?(\d{4})"/>
         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
         <input type="Submit" />
     </form>
